61516b5ce82d2e0305576f145274aaa2c0239d2a
Mike Bethany <mikbe.tk@gmail.com>
2011-05-12 18:26:05 -0400

    better fix testing

--- lib/chicken_little/patch.rb
+++ lib/chicken_little/patch.rb
@@ -5,9 +5,29 @@ module ChickenLittle
   class Patch
     extend Commandable
     
-    command "Disables the annoying deprication warnings from the gem command", :xor
+    command "Tries to fix your gems so they stop giving the deprication warning."
     def install
-      describe_better_fix
+       
+      (1..5).each do |i|
+        output = capture_output{
+          puts "Cleaning up gems, this will take a while. Go get some coffee..."
+          case i
+            when 1,4
+              `gem pristine --all --no-extensions`
+            else
+              `gem pristine --all`
+          end 
+        }
+        puts "stdout: \n#{output[:stdout]}\n\n"
+        puts "stderr: \n#{output[:stderr]}\n"
+        exit!
+      end
+      
+    end
+    
+    command "Forces the old method for disabling the annoying deprication warnings", :xor
+    def force_install
+      describe_fix
       supported?
       return "Chicken Little is already installed" if installed?
       new_spec_file = spec_file_contents.collect { |line| line.index(Uncommented_Line) ? "##{line.strip}\n" : line}
@@ -46,33 +66,31 @@ module ChickenLittle
     end
     
     command "Desribe a better way to fix the problem"
-    def describe_fix(force_description=true)
-      flag_file = "#{File.expand_path(__FILE__)}/better_fix_described"
-      unless File.exist? flag_file
+    def describe_fix(force_description=false)
+      flag_file = "#{File.expand_path(File.dirname(__FILE__))}/better_fix_described"
+      unless File.exist?(flag_file) && !force_description
         FileUtils.touch flag_file
-        abort %{\nImportant! This app is not necessary.\nA better fix is to run\n$ gem pristine --all --no-extensions\nThen run:\n$ gem pristine --all\nYou may need to run those commands several times.\nYou can have also have Chicken Little run them for you with:\n$ chicken_little better_fix\nIt will run the commands several times till the errors go away or till it's tried several times.\nIf you still have problems you can then run the install command again.}
-      end
-    end
+        puts %{
+  Important! The old install method should not be necessary.
 
-    command "The proper way to fix the deprication errors"
-    def better_fix
-      
-      (1..5).each do |i|
-        output = capture_output{
-          puts "Cleaning up gems, this will take a while. Go get some coffee..."
-          case i
-            when 1,4
-              `gem pristine --all --no-extensions`
-            else
-              `gem pristine --all`
-          end 
-        }
-        puts "stdout: \n#{output[:stdout]}\n\n"
-        puts "stderr: \n#{output[:stderr]}\n"
+  A better fix is to run:
+  $ gem pristine --all --no-extensions
+
+  Then run this:
+  $ gem pristine --all
+
+  You may need to run those commands several times.
+
+  Chicken Little is now running those methods for you. This will take a while.
+  They will be run until the errors go away or it's tried too many times.
+  
+  If you still have problems you can then force the old install method with:
+  $ chicken_little force_install
+          
+          }
+          exit!
       end
-      
     end
-    
 
     private
 
