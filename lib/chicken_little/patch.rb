e37c997ed69a752138471bbfc5756a6750659f44
Mike Bethany <mikbe.tk@gmail.com>
2011-05-12 17:56:18 -0400

    test better_fix

--- lib/chicken_little/patch.rb
+++ lib/chicken_little/patch.rb
@@ -5,8 +5,9 @@ module ChickenLittle
   class Patch
     extend Commandable
     
-    command "Disables the annoying deprication warnings from the gem command", :default, :xor
+    command "Disables the annoying deprication warnings from the gem command", :xor
     def install
+      describe_better_fix
       supported?
       return "Chicken Little is already installed" if installed?
       new_spec_file = spec_file_contents.collect { |line| line.index(Uncommented_Line) ? "##{line.strip}\n" : line}
@@ -44,7 +45,49 @@ module ChickenLittle
       "Looks good. Chicken Little can find and modify the necessary files."
     end
     
+    command "Desribe a better way to fix the problem"
+    def describe_fix(force_description=true)
+      flag_file = "#{File.expand_path(__FILE__)}/better_fix_described"
+      unless File.exist? flag_file
+        FileUtils.touch flag_file
+        abort %{\nImportant! This app is not necessary.\nA better fix is to run\n$ gem pristine --all --no-extensions\nThen run:\n$ gem pristine --all\nYou may need to run those commands several times.\nYou can have also have Chicken Little run them for you with:\n$ chicken_little better_fix\nIt will run the commands several times till the errors go away or till it's tried several times.\nIf you still have problems you can then run the install command again.}
+      end
+    end
+
+    command "The proper way to fix the deprication errors"
+    def better_fix
+      
+      (1..5).each do |i|
+        output = capture_output{
+          puts "Cleaning up gems, this will take a while. Go get some coffee..."
+          case i
+            when 1,4
+              `gem pristine --all --no-extensions`
+            else
+              `gem pristine --all`
+          end 
+        }
+        puts "stdout: \n#{output[:stdout]}\n\n"
+        puts "stderr: \n#{output[:stderr]}\n"
+      end
+      
+    end
+    
+
     private
+
+    def capture_output
+      begin
+        require 'stringio'
+        $o_stdout, $o_stderr = $stdout, $stderr
+        $stdout, $stderr = StringIO.new, StringIO.new
+        yield
+        {:stdout => $stdout.string, :stderr => $stderr.string}
+      ensure
+        $stdout, $stderr = $o_stdout, $o_stderr
+      end
+    end
+
     
     def gem_file_path
       @gem_file ||= `which gem`.chomp
--- lib/chicken_little/version.rb
+++ lib/chicken_little/version.rb
@@ -1,3 +1,3 @@
 module ChickenLittle
-  VERSION = "0.1.1"
+  VERSION = "0.2.0"
 end
